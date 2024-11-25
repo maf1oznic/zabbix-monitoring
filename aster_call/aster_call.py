9d62068fa37c61bf2e1c588fedbe24369537cfa5
maf1oznic <127044787+maf1oznic@users.noreply.github.com>
2024-11-25 16:56:52 +0300

    Create aster_call.py

---
+++ aster_call/aster_call.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+
+import telnetlib
+import time
+import sys
+
+# Настройки AMI
+AMI_HOST = "ip addr"
+AMI_PORT = 5038
+AMI_USER = "admin"
+AMI_PASSWORD = "pwd"
+
+trunk = "trunk_name"
+
+def send_action(action):
+    """Отправляет действие на AMI"""
+    tn.write(action.encode('utf-8') + b'\n\n')
+    time.sleep(0.5)
+    return tn.read_very_eager().decode('utf-8')
+
+
+# Проверяем, что переданы три аргумента
+if len(sys.argv) < 4:
+    print("Usage: aster_call.py <phone_number> <event_status> <event_name>")
+    sys.exit(1)
+
+# Получаем данные из аргументов командной строки
+number = sys.argv[1]  # {ALERT.SENDTO}
+event_status = sys.argv[2]  # {EVENT.STATUS}
+event_name = sys.argv[3]  # {EVENT.NAME}
+
+# Подключаемся к AMI
+tn = telnetlib.Telnet(AMI_HOST, AMI_PORT)
+tn.read_until(b"Asterisk Call Manager/")
+
+tn.write(
+    b"Action: Login\nUsername: " + AMI_USER.encode('utf-8') + b"\nSecret: " + AMI_PASSWORD.encode('utf-8') + b"\n\n"
+)
+tn.read_very_eager()
+
+# Формируем запрос на вызов
+action = f"""Action: Originate
+Channel: PJSIP/{number}@{trunk}
+Context: gtts-test
+Exten: s
+Priority: 1
+CallerID: {number}
+Timeout: 30000
+Async: true
+Variable: EVENT_STATUS={event_status}
+Variable: EVENT_NAME={event_name}
+"""
+
+# Отправляем действие на Asterisk
+response = send_action(action)
+print(f"Dialing {number} with status '{event_status}' and event '{event_name}' through {trunk}... Response: {response}")
+
+# Завершаем сессию
+tn.write(b"Action: Logoff\n\n")
+tn.close()
